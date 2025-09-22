8730310e21c91f25170670e97d7bb7c250bb8a74
Topst Developer <topstdeveloper@gmail.com>
2025-09-22 16:29:12 +0900

    [patch] add --fwdn boot-firmware/tcc750x_fwdn.json option in fwdn_nd.sh

--- fwdn_ai/fwdn_aig.sh
+++ fwdn_ai/fwdn_aig.sh
@@ -9,16 +9,16 @@ port_num=$num
 echo ""
 
 echo "Step 1. Connect between Host PC and TOPST AI-G"
-./fwdn_aig --fwdn boot-firmware/tcc750x_fwdn.json --port "$port_num"
+./fwdn_ai --fwdn boot-firmware/tcc750x_fwdn.json --port "$port_num"
 
 echo "Step 2. Low-format"
-./fwdn_aig --storage emmc --low-format --port "$port_num"
+./fwdn_ai --fwdn boot-firmware/tcc750x_fwdn.json --storage emmc --low-format --port "$port_num"
 
 echo "Step 3. Download boot-firmware"
-./fwdn_aig -w boot-firmware/tcc750x_boot.json --port "$port_num"
+./fwdn_ai --fwdn boot-firmware/tcc750x_fwdn.json -w boot-firmware/tcc750x_boot.json --port "$port_num"
 
 echo "Step 4. Download FAI file (system image)"
-./fwdn_aig -w output_aig.fai --storage emmc --area user --port "$port_num"
+./fwdn_ai --fwdn boot-firmware/tcc750x_fwdn.json -w output_aig.fai --storage emmc --area user --port "$port_num"
 
 if [ $? -eq 0 ]; then
   echo "TOPST AI-G FWDN is complete!"
